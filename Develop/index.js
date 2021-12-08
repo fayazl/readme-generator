2d209be18c89addc8fca6a7698842609addf6e7d
Fayaz <flalani100@gmail.com>
2021-12-07 22:13:51 -0500

    Included packages for application and require variables

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,9 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
+const util = require('util')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
