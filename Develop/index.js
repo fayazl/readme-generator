b4a8efaa41bc8b4deb0f142e2d7bc1bc6063a992
Fayaz <flalani100@gmail.com>
2021-12-07 23:33:15 -0500

    Added Questions and Prompts

--- Develop/index.js
+++ Develop/index.js
@@ -6,13 +6,72 @@ const generateMarkdown = require('./utils/generateMarkdown')
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () =>{
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project?"
+    
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please provide a description of your project"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Please provide the installation instructions"
+        },    
+        {
+            type: "input",
+            name: "usage",
+            message: "Please provide the usage information of your project"
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Please provide the contribution guidelines"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "What are the test instructions"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Choose the right license for this project:",
+            choices: [
+                "Apache",
+                "Academic",
+                "Intel",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?"
+        }
+    ]);
+};
+
+questions()
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
