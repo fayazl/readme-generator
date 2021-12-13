23deb6f919f20d8bc6a4f78d283e0be8efa27574
Fayaz <flalani100@gmail.com>
2021-12-13 18:43:06 -0500

    Commit with all questions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -16,19 +16,35 @@ function generateMarkdown(data) {
   
   <h1>### ${data.title}</h1>
 
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)<br />
+
   ## Table of Contents
 - [Description](#description)
 - [Installation](#installation)
 - [Usage](#usage)
-- [License](#license)
 - [Contributing](#contributing)
 - [Tests](#tests)
 - [Questions](#questions)
 
+  <h2>##Description</h2>
+  ${data.description}
 
-  <h2>### ${data.description}</h2>
+  <h2>##Installation</h2>
+  ### ${data.installation}
 
-  <h2>### ${data.installation}</h2>
+  <h2>##Usage</h2>
+  ### ${data.usage}
+
+  <h2>##Contributing<h2>
+  ### ${data.contributing}
+
+  <h2>##Tests</h2>
+  ### ${data.tests}
+
+  <h2>##Questions</h2>
+  ### https://github.com/${data.github}
+
+  ### If you would like to reach me, please email me at ${data.email}
 
 `;
 }
